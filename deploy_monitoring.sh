d30dca824af4265e072ed85c88791c750cb429da
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-27 19:23:42 +0530

    fix

--- deploy_monitoring.sh
+++ deploy_monitoring.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 MANIFEST_DIR="k8s"
-
+helm_bin="/usr/local/bin/helm"
 echo "Deploying Helm"
 #### Install Helm ####
 curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 /tmp/get_helm.sh
 bash /tmp/get_helm.sh
+${helm_bin} repo add stable https://charts.helm.sh/stable
+${helm_bin} repo update
 #### Install Helm ####
 
 echo "Deploying prometheus"
@@ -16,7 +18,7 @@ kind: Namespace
 metadata:
   name: prometheus
 EOF
-/usr/local/bin/helm install prometheus --namespace prometheus stable/prometheus --version 10.3.1 -f ${MANIFEST_DIR}/prometheus.yaml
+${helm_bin} install prometheus --namespace prometheus stable/prometheus --version 10.3.1 -f ${MANIFEST_DIR}/prometheus.yaml
 
 
 echo "Deploying Grafana"
@@ -27,6 +29,6 @@ kind: Namespace
 metadata:
   name: grafana
 EOF
-/usr/local/bin/helm install grafana --namespace grafana stable/grafana --version 4.3.2 -f ${MANIFEST_DIR}/grafana.yaml
+${helm_bin} install grafana --namespace grafana stable/grafana --version 4.3.2 -f ${MANIFEST_DIR}/grafana.yaml
 grafana_password=$(kubectl get secret -n grafana grafana -o jsonpath="{.data.admin-password}" | base64 --decode)
 echo "Log into Grafana using user: admin & password: ${grafana_password}"
