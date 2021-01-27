6239ec8edcc76e5f01eb186f40e04ead8dca4e20
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-27 19:19:21 +0530

    chnage

--- deploy_monitoring.sh
+++ deploy_monitoring.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 MANIFEST_DIR="k8s"
-echo "Deploying prometheus"
 
+echo "Deploying Helm"
 #### Install Helm ####
 curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 /tmp/get_helm.sh
 bash /tmp/get_helm.sh
 #### Install Helm ####
 
+echo "Deploying prometheus"
 echo "Creating Namespace: prometheus"
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -15,7 +16,7 @@ kind: Namespace
 metadata:
   name: prometheus
 EOF
-helm install prometheus --namespace prometheus stable/prometheus --version 10.3.1 -f ${MANIFEST_DIR}/prometheus.yaml
+/usr/local/bin/helm install prometheus --namespace prometheus stable/prometheus --version 10.3.1 -f ${MANIFEST_DIR}/prometheus.yaml
 
 
 echo "Deploying Grafana"
@@ -26,6 +27,6 @@ kind: Namespace
 metadata:
   name: grafana
 EOF
-helm install grafana --namespace grafana stable/grafana --version 4.3.2 -f ${MANIFEST_DIR}/grafana.yaml
+/usr/local/bin/helm install grafana --namespace grafana stable/grafana --version 4.3.2 -f ${MANIFEST_DIR}/grafana.yaml
 grafana_password=$(kubectl get secret -n grafana grafana -o jsonpath="{.data.admin-password}" | base64 --decode)
 echo "Log into Grafana using user: admin & password: ${grafana_password}"
