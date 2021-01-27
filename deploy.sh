20b60249c177dac4e414e106872730ac22b0fa4a
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-27 18:48:22 +0530

    ssl

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
+if [[ -z "$1" ]]; then
+    action="deploy"
+fi
+
+if [ "$1" = "deploy"  ]; then
+    action="deploy"
+elif [ "$1" = "deployssl"  ]; then
+    action="deployssl"
+else
+    echo "Choose deploy OR deployssl"
+    exit 0
+fi
+echo "Action : ${action}"
 
 MANIFEST_DIR="k8s"
 NAMESPACE="test-rabbitmq"
@@ -18,11 +31,18 @@ EOF
 echo "Deploying RBAC"
 kubectl apply -f $MANIFEST_DIR/rbac.yaml -n ${NAMESPACE}
 
-echo "Deploying Configmap for setting rabbitmq.conf"
-kubectl apply -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
-
-echo "Deploying Services"
-kubectl apply -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
-
-echo "Deploying RabbitMQ Stateful Set"
-kubectl apply -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
+if [ "$action" = "deploy"  ]; then
+    echo "Deploying Configmap for setting rabbitmq.conf"
+    kubectl apply -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
+    echo "Deploying Services"
+    kubectl apply -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
+    echo "Deploying RabbitMQ Stateful Set"
+    kubectl apply -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
+else
+    echo "Deploying Configmap for setting rabbitmq.conf with SSL"
+    kubectl apply -f $MANIFEST_DIR/configmap_ssl.yaml -n ${NAMESPACE}
+    echo "Deploying Services with SSL"
+    kubectl apply -f $MANIFEST_DIR/services_ssl.yaml -n ${NAMESPACE}
+    echo "Deploying RabbitMQ Stateful Set with SSL"
+    kubectl apply -f $MANIFEST_DIR/statefulset.yaml_ssl -n ${NAMESPACE}
+fi
--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+#!/bin/bash
+set -e
+
+if [ "$1" = "deploy"  ]; then
+    action="deploy"
+elif [ "$1" = "deployssl"  ]; then
+    action="deployssl"
+else
+    echo "Choose deploy OR deployssl"
+    exit 0
+fi
+echo "Action : ${action}"
+
 MANIFEST_DIR="k8s"
 NAMESPACE="test-rabbitmq"
 
@@ -10,14 +23,21 @@ kubectl get nodes
 echo "Deleting RBAC"
 kubectl delete -f $MANIFEST_DIR/rbac.yaml -n ${NAMESPACE}
 
-echo "Deleting Configmap"
-kubectl delete -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
-
-echo "Deleting Services"
-kubectl delete -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
-
-echo "Deleting RabbitMQ Stateful Set"
-kubectl delete -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
+if [ "$action" = "deploy"  ]; then
+    echo "Deleting Configmap"
+    kubectl delete -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
+    echo "Deleting Services"
+    kubectl delete -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
+    echo "Deleting RabbitMQ Stateful Set"
+    kubectl delete -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
+else
+    echo "Deploying Configmap for setting rabbitmq.conf with SSL"
+    kubectl delete -f $MANIFEST_DIR/configmap_ssl.yaml -n ${NAMESPACE}
+    echo "Deploying Services with SSL"
+    kubectl delete -f $MANIFEST_DIR/services_ssl.yaml -n ${NAMESPACE}
+    echo "Deploying RabbitMQ Stateful Set with SSL"
+    kubectl delete -f $MANIFEST_DIR/statefulset.yaml_ssl -n ${NAMESPACE}
+fi
 
 echo "Deleting Namespace: ${NAMESPACE}"
 cat <<EOF | kubectl delete -f -
