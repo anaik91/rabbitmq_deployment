bcaf114fed72da43b9befe4221d45da4b744e3a9
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-27 17:39:24 +0530

    fix

--- destroy.sh
+++ destroy.sh
@@ -7,22 +7,22 @@ NAMESPACE="test-rabbitmq"
 echo "Listing Nodes"
 kubectl get nodes
 
-echo "Creating Namespace: ${NAMESPACE}"
+echo "Deleting RBAC"
+kubectl delete -f $MANIFEST_DIR/rbac.yaml -n ${NAMESPACE}
+
+echo "Deleting Configmap"
+kubectl delete -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
+
+echo "Deleting Services"
+kubectl delete -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
+
+echo "Deleting RabbitMQ Stateful Set"
+kubectl delete -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
+
+echo "Deleting Namespace: ${NAMESPACE}"
 cat <<EOF | kubectl delete -f -
 apiVersion: v1
 kind: Namespace
 metadata:
   name: ${NAMESPACE}
 EOF
-
-echo "Deploying RBAC"
-kubectl delete -f $MANIFEST_DIR/rbac.yaml -n ${NAMESPACE}
-
-echo "Deploying Configmap for setting rabbitmq.conf"
-kubectl delete -f $MANIFEST_DIR/configmap.yaml -n ${NAMESPACE}
-
-echo "Deploying Services"
-kubectl delete -f $MANIFEST_DIR/services.yaml -n ${NAMESPACE}
-
-echo "Deploying RabbitMQ Stateful Set"
-kubectl delete -f $MANIFEST_DIR/statefulset.yaml -n ${NAMESPACE}
